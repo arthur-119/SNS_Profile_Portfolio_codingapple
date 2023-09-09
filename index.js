0e9ca43589bb941a7aeafcce026729f946e2079e
Seungwoo <im.arthur119@gmail.com>
2023-09-09 16:04:42 +0900

    sns프로필 포트폴리오

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var button = document.getElementById("functionAlert");
+    button.addEventListener('click', function() {
+        alert('버튼만든김에 js까지 연결해봄')
+    })
+}
