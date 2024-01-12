c5280c8d9f97c93a470e8cc71317bfd2bbec52dd
KangSeungwoo <im.arthur119@gmail.com>
2024-01-13 00:45:49 +0900

    대학생으로 수정 / 인스타 프로필링크 연결

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.onload = function() {
-    var button = document.getElementById("functionAlert");
+    var button = document.getElementById("contact");
+    var link = 'https://www.instagram.com/im.arthur119/';
     button.addEventListener('click', function() {
-        alert('버튼만든김에 js까지 연결해봄')
+        // alert('버튼만든김에 js까지 연결해봄')
+        window.open(link)
     })
 }
